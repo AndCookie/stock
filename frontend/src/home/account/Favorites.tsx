ad72bff336dace3e0af8a9ec7d562521aaff18db
AndCookie <jamie9@naver.com>
2024-10-31 14:12:05 +0900

    [FE] Feat: Home/Account

---
+++ frontend/src/home/account/Favorites.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import styles from './Favorites.module.css';
+
+const Favorites = () => {
+  return (
+    <div>
+      <h1>관심 종목</h1>
+      {/* Add the specific content and styling for 관심 종목 */}
+    </div>
+  );
+};
+
+export default Favorites;
---
+++ frontend/src/home/account/History.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import styles from './History.module.css';
+
+const History = () => {
+  return (
+    <div>
+      <h1>주문 내역</h1>
+      {/* Add the specific content and styling for 주문 내역 */}
+    </div>
+  );
+};
+
+export default History;
