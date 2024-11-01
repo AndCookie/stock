5159b4d8e9289d30e52e38a2f4319ce530899014
임광영 <kokoz22@naver.com>
2024-11-01 10:50:04 +0900

    [FE] Refactor: Splite Store using Zustand - useIndexStore을 분리해서 store에서 관리합니다.

--- frontend/src/store/useIndexStore.ts
+++ frontend/src/store/useIndexStore.ts
@@ -1,1 +1,22 @@
+import axios from "axios";
 import { create } from "zustand";
+
+export const useIndexStore = create((set) => ({
+  indexData: {},
+
+  fetchIndexData: async (indexType: string) => {
+    try {
+      const response = await axios.get("http://localhost:3000/api/v1/indexDetail", {
+        params: {
+          indexType: indexType,
+        },
+      });
+
+      set(() => ({
+        indexData: response.data,
+      }));
+    } catch (error) {
+      console.log(error);
+    }
+  },
+}));
