81229585393b7420888d75238f57f59f1cd2f412
woojinchoi <woojinchoi1023@gmail.com>
2024-11-04 16:29:17 +0900

    [FE] Fix: linter error fix - 빌드 시 발생하는 린터 에러를 수정했습니다.

--- frontend/src/pages/MarketPage.tsx
+++ frontend/src/pages/MarketPage.tsx
@@ -1,20 +1,29 @@
-import { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useState } from "react";
+import { useParams } from "react-router-dom";
 
-import IndexChart from '../market/IndexChart';
+import IndexChart from "../market/IndexChart";
 // import styles from './MarketPage.module.css';
 
 const MarketPage = () => {
   const { indexTypeId } = useParams();
-  const indexTypes = ['국내', '해외', '환율', '원자재']
+  const indexTypes = ["국내", "해외", "환율", "원자재"];
 
-  const [selectedIndex, setSelectedIndex] = useState(indexTypes[indexTypeId]);
+  // const [selectedIndex, setSelectedIndex] = useState(indexTypes[indexTypeId]);
+
+  // indexTypeId를 숫자로 변환하고 유효성 검사를 수행
+  const indexId =
+    indexTypeId !== undefined && !isNaN(Number(indexTypeId))
+      ? Number(indexTypeId)
+      : 0; // 기본값을 0으로 설정 (국내)
+  const [selectedIndex, setSelectedIndex] = useState(indexTypes[indexId]);
 
   return (
     <>
       <div>오늘의 지수</div>
       {indexTypes.map((indexType) => (
-        <button key={indexType} onClick={() => setSelectedIndex(indexType)}>{indexType}</button>
+        <button key={indexType} onClick={() => setSelectedIndex(indexType)}>
+          {indexType}
+        </button>
       ))}
       <IndexChart indexType={selectedIndex} />
     </>
