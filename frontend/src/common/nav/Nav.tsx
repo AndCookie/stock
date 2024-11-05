9d22f42890c9a08007f3bbcae27d6e63c79db6ac
woojinchoi <woojinchoi1023@gmail.com>
2024-11-05 16:34:59 +0900

    [FE] Feat: Search nav - 네비게이션 바에 검색 기능을 구현했습니다.

--- frontend/src/common/nav/Nav.tsx
+++ frontend/src/common/nav/Nav.tsx
@@ -1,17 +1,20 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import styles from './Nav.module.css';
-import { FaSearch } from 'react-icons/fa';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./Nav.module.css";
+import { FaSearch } from "react-icons/fa";
 
-import member from '../../assets/images/member.png';
-import chatBot from '../../assets/images/chatBot.png';
-import heatMap from '../../assets/images/heatMap.png';
+import member from "../../assets/images/member.png";
+import chatBot from "../../assets/images/chatBot.png";
+import heatMap from "../../assets/images/heatMap.png";
 
-import MyPage from './myPage/myPage';
-import ChatBot from './chatBot/ChatBot';
-import HeatMap from './heatMap/HeatMap';
+import MyPage from "./myPage/myPage";
+import ChatBot from "./chatBot/ChatBot";
+import HeatMap from "./heatMap/HeatMap";
+import Search from "./Search";
 
-const MODAL_COMPONENTS: { [key: string]: React.ComponentType<{ closeModal: () => void }> } = {
+const MODAL_COMPONENTS: {
+  [key: string]: React.ComponentType<{ closeModal: () => void }>;
+} = {
   chatBot: ChatBot,
   heatMap: HeatMap,
   myPage: MyPage,
@@ -28,21 +31,39 @@ const Nav: React.FC = () => {
     <>
       {/* Header 영역 */}
       <header className={styles.header}>
-        <div className={styles.logo} onClick={() => navigate('/')}>
+        <div className={styles.logo} onClick={() => navigate("/")}>
           <span className={styles.modu}>모두</span>모투
         </div>
         <div className={styles.searchContainer}>
           <FaSearch className={styles.searchIcon} />
-          <input className={styles.search} placeholder="주식, 메뉴, 종목코드를 검색하세요" />
+          <Search />
         </div>
         <div className={styles.icons}>
-          <img className={styles.icon1} src={chatBot} alt="chatBot" onClick={handleOpenModal('chatBot')} />
-          <img className={styles.icon2} src={heatMap} alt="heatMap" onClick={handleOpenModal('heatMap')} />
-          <img className={styles.icon3} src={member} alt="member" onClick={handleOpenModal('myPage')} />
+          <img
+            className={styles.icon1}
+            src={chatBot}
+            alt="chatBot"
+            onClick={handleOpenModal("chatBot")}
+          />
+          <img
+            className={styles.icon2}
+            src={heatMap}
+            alt="heatMap"
+            onClick={handleOpenModal("heatMap")}
+          />
+          <img
+            className={styles.icon3}
+            src={member}
+            alt="member"
+            onClick={handleOpenModal("myPage")}
+          />
         </div>
       </header>
 
-      {openModal && React.createElement(MODAL_COMPONENTS[openModal], { closeModal: handleCloseModal })}
+      {openModal &&
+        React.createElement(MODAL_COMPONENTS[openModal], {
+          closeModal: handleCloseModal,
+        })}
     </>
   );
 };
