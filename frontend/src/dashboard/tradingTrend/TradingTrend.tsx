9558989ec519eb73cf1cee2e12fec0e66a80b196
최병주 <cbju98@gmail.com>
2024-11-05 15:04:05 +0900

    [FE] Fix: Button in Info Widget and TradingTrend Widget - DashboardPage.tsx에서 위젯 드래그 이벤트가 위젯 내 버튼 클릭 이벤트보다 우선시되지 않도록 막았습니다.

--- frontend/src/dashboard/tradingTrend/TradingTrend.tsx
+++ frontend/src/dashboard/tradingTrend/TradingTrend.tsx
@@ -1,27 +1,44 @@
-// 거래동향 / 거래원 /투자자
+// 거래동향, 거래원, 투자자
 
 import { useState } from "react";
 import Daily from "./Daily";
 import Trader from "./Trader";
 import Investor from "./Investor";
 
-const TradingTrend = () => {
-  const TABS = [
-    { label: "거래동향", component: <Daily /> },
-    { label: "거래원", component: <Trader /> },
-    { label: "투자자", component: <Investor /> },
-  ];
+type TradingTrendProps = {
+  setIsDraggable: (value: boolean) => void; // 드래그 활성화 제어 함수
+};
+
+const TABS = [
+  { label: "거래동향", component: <Daily /> },
+  { label: "거래원", component: <Trader /> },
+  { label: "투자자", component: <Investor /> },
+];
+
+const TradingTrend = ({ setIsDraggable }: TradingTrendProps) => {
   const [selectedTab, setSelectedTab] = useState(0);
 
   return (
     <div>
+      {/* 탭 */}
       <div>
         {TABS.map((tab, index) => (
-          <button key={index} onClick={() => setSelectedTab(index)}>
+          <button
+            key={index}
+            onMouseDown={(event) => {
+              event.stopPropagation(); // 클릭 시 드래그 방지
+              setIsDraggable(false); // 버튼 클릭 시 드래그 비활성화
+            }}
+            onClick={() => {
+              setSelectedTab(index);
+              setIsDraggable(true); // 클릭 후 드래그 다시 활성화
+            }}
+          >
             {tab.label}
           </button>
         ))}
       </div>
+      {/* 선택된 탭의 컴포넌트 렌더링 */}
       <div>{TABS[selectedTab].component}</div>
     </div>
   );
