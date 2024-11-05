e06651a7da9e014a784bb1fac18c078b44b1ba13
woojinchoi <woojinchoi1023@gmail.com>
2024-11-05 10:39:20 +0900

    [FE] Fix: rename MarketPage.module.css - 대문자로 수정했습니다

--- frontend/src/pages/MarketPage.tsx
+++ frontend/src/pages/MarketPage.tsx
@@ -1,13 +1,16 @@
-import { useState } from 'react';
-import { useParams } from 'react-router-dom';
-import IndexChart from '../market/IndexChart';
-import styles from './MarketPage.module.css';
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import IndexChart from "../market/IndexChart";
+import styles from "./MarketPage.module.css";
 
 const MarketPage = () => {
   const { indexTypeId } = useParams();
-  const indexTypes = ['국내', '해외', '환율', '원자재'];
-  const initialIndex = indexTypeId && indexTypes[parseInt(indexTypeId, 10)] ? indexTypes[parseInt(indexTypeId, 10)] : '국내';
-  
+  const indexTypes = ["국내", "해외", "환율", "원자재"];
+  const initialIndex =
+    indexTypeId && indexTypes[parseInt(indexTypeId, 10)]
+      ? indexTypes[parseInt(indexTypeId, 10)]
+      : "국내";
+
   const [selectedIndex, setSelectedIndex] = useState(initialIndex);
 
   return (
@@ -18,7 +21,9 @@ const MarketPage = () => {
           {indexTypes.map((type) => (
             <button
               key={type}
-              className={`${styles.tab} ${selectedIndex === type ? styles.activeTab : ''}`}
+              className={`${styles.tab} ${
+                selectedIndex === type ? styles.activeTab : ""
+              }`}
               onClick={() => setSelectedIndex(type)}
             >
               {type}
