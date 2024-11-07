efb2f621143f81ca12a932f7a361983811b3b27c
최병주 <cbju98@gmail.com>
2024-11-07 13:42:00 +0900

    [FE] Feat: Make Socket Store - 웹소켓 전역 상태 관리 store 만들었습니다.

--- frontend/src/dashboard/trading/order/Buy.tsx
+++ frontend/src/dashboard/trading/order/Buy.tsx
@@ -1,11 +1,20 @@
-// 주문하기 - 구매
+// src/components/Buy.tsx
+import React from "react";
+import useSocketStore from "../../../store/useSocketStore";
 
-const Buy = () => {
-  return(
+const Buy: React.FC = () => {
+  // Zustand 스토어에서 tradingData의 STCK_PRPR 값 가져오기
+  const marketPrice = useSocketStore((state) => state.tradingData?.STCK_PRPR);
+
+  return (
     <div>
-      Buy
+      <h2>구매 화면</h2>
+      <div>
+        <h3>시장가</h3>
+        <p>{marketPrice ? `${marketPrice} 원` : "데이터 없음"}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Buy
+export default Buy;
