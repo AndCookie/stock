d9fdcddf8e7e377f1437b751a21dc02c276fcaa2
Thoughtful-Lover <isso8336@gmail.com>
2024-11-11 14:09:02 +0900

    :sparkles: Feat: stock ranking stock info mock data

--- frontend/src/home/stockRanking/StockRanking.tsx
+++ frontend/src/home/stockRanking/StockRanking.tsx
@@ -1,20 +1,77 @@
-import styles from "./StockRanking.module.css";
-import StockInfo from "./StockInfo";
-import { useState } from "react";
+import styles from './StockRanking.module.css';
+import StockInfo from './StockInfo';
+import { useState } from 'react';
+import samsungLogo from '../../assets/images/samsung.png';
 
 const StockRanking = () => {
   // const data = useIndexData()
-  const [stockData] = useState({
-    dataRank: "1",
-    stockName: "삼성전자",
-    stockPrice: "100,000",
-    previousDayVersusPrice: "40,000",
-    previousDayVersus: "60",
-  });
+  const [selectedCategory, setSelectedCategory] = useState('거래 대금');
+
+  const categories = ['거래 대금', '거래량', '급상승', '급하락'];
+
+  // 이거 각각의 세부 카테고리 호출할 때마다 axios로 데이터 저장
+  const [stockData] = useState([
+    {
+      dataRank: '1',
+      stockName: '삼성전자',
+      logoLink: samsungLogo,
+      stockPrice: '100,000',
+      previousDayVersusPrice: '40,000',
+      previousDayVersus: '60',
+    },
+    {
+      dataRank: '1',
+      stockName: '삼성전자',
+      logoLink: samsungLogo,
+      stockPrice: '100,000',
+      previousDayVersusPrice: '40,000',
+      previousDayVersus: '60',
+    },
+    {
+      dataRank: '1',
+      stockName: '삼성전자',
+      logoLink: samsungLogo,
+      stockPrice: '100,000',
+      previousDayVersusPrice: '40,000',
+      previousDayVersus: '60',
+    },
+    {
+      dataRank: '1',
+      stockName: '삼성전자',
+      logoLink: samsungLogo,
+      stockPrice: '100,000',
+      previousDayVersusPrice: '40,000',
+      previousDayVersus: '60',
+    },
+    {
+      dataRank: '1',
+      stockName: '삼성전자',
+      logoLink: samsungLogo,
+      stockPrice: '100,000',
+      previousDayVersusPrice: '40,000',
+      previousDayVersus: '60',
+    },
+  ]);
+
   return (
     <div className={styles.container}>
       StockRanking
-      <StockInfo {...stockData} />
+      <div className="categoryTabs">
+        {categories.map((category) => (
+          <button
+            key={category}
+            className={`${'tabButton'} ${selectedCategory === category ? 'activeTab' : ''}`}
+            onClick={() => setSelectedCategory(category)}
+          >
+            {category}
+          </button>
+        ))}
+      </div>
+      <div className={styles.stockData}>
+        {stockData.map((stockinfo, index) => (
+          <StockInfo key={index} {...stockinfo} />
+        ))}
+      </div>
     </div>
   );
 };
