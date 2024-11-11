353d95157a3f74e1a20e9f0bd3ad3a7436518d14
AndCookie <jamie9@naver.com>
2024-11-11 16:20:45 +0900

    [FE] Design: dashboard/OrderBook 호가 - 테이블 구조 변경 - 이것은 디자인인가 기능 구현인가.. - useSocketStore.ts 데이터 연결

--- frontend/src/dashboard/orderBook/OrderBook.tsx
+++ frontend/src/dashboard/orderBook/OrderBook.tsx
@@ -1,271 +1,91 @@
-// 호가
+import React from 'react';
+import useSocketStore from '../../store/useSocketStore';
+import styles from './OrderBook.module.css';
 
-const OrderBook = () => {
-  // TODO: data 연결
-  // TODO: 상한가 하한가 정보 받아와야함...
-  const { orderBookData, tradingData } = {
-    orderBookData: {
-      ASKP1: 2000,
-      ASKP2: 2100,
-      ASKP3: 2200,
-      ASKP4: 2300,
-      ASKP5: 2400,
-      ASKP6: 2500,
-      ASKP7: 2600,
-      ASKP8: 2700,
-      ASKP9: 2800,
-      ASKP10: 2900,
-      BIDP1: 1900,
-      BIDP2: 1800,
-      BIDP3: 1700,
-      BIDP4: 1600,
-      BIDP5: 1500,
-      BIDP6: 1400,
-      BIDP7: 1300,
-      BIDP8: 1200,
-      BIDP9: 1100,
-      BIDP10: 1000,
-      ASKP_RSQN1: 1,
-      ASKP_RSQN2: 2,
-      ASKP_RSQN3: 3,
-      ASKP_RSQN4: 4,
-      ASKP_RSQN5: 5,
-      ASKP_RSQN6: 6,
-      ASKP_RSQN7: 7,
-      ASKP_RSQN8: 8,
-      ASKP_RSQN9: 9,
-      ASKP_RSQN10: 10,
-      BIDP_RSQN1: 1,
-      BIDP_RSQN2: 2,
-      BIDP_RSQN3: 3,
-      BIDP_RSQN4: 4,
-      BIDP_RSQN5: 5,
-      BIDP_RSQN6: 6,
-      BIDP_RSQN7: 7,
-      BIDP_RSQN8: 8,
-      BIDP_RSQN9: 9,
-      BIDP_RSQN10: 10,
-      TOTAL_ASKP_RSQN: 55,
-      TOTAL_BIDP_RSQN: 55,
-    },
-    tradingData: [
-      {
-        STCK_CNTG_HOUR: 111858,
-        STCK_PRPR: 2000, // 체결가
-        CNTG_VOL: 10, // 체결량
-        ACML_VOL: 123,
-        CTTR: 73.3,
-        CCLD_DVSN: 1,
-      },
-      {
-        STCK_CNTG_HOUR: 130105,
-        STCK_PRPR: 1900,
-        CNTG_VOL: 30,
-        ACML_VOL: 153,
-        CTTR: 69.84,
-        CCLD_DVSN: 5,
-      },
-      {
-        STCK_CNTG_HOUR: 142251,
-        STCK_PRPR: 2000,
-        CNTG_VOL: 20,
-        ACML_VOL: 173,
-        CTTR: 71.48, // 강도는 마지막게 가장 최신입니다.
-        CCLD_DVSN: 1,
-      },
-    ],
-  };
+const OrderBook: React.FC = () => {
+  const { orderBookData, tradingData } = useSocketStore();
+  const maxAskPrice = 76800; // 상한가
+  const minBidPrice = 41400; // 하한가
+  const currentPrice = 56300; // 기준 가격
+  const maxPrice = 58200; // 최고가
+  const minPrice = 56600; // 최저가
+  const quantity = 31499922; // 거래량
+
+  if (!orderBookData || !tradingData) {
+    return <div className={styles.loading}>Loading...</div>;
+  }
 
   return (
-    <div>
-      <div>호가</div>
-      <table>
-        <tbody>
-          <tr>
-            <td>{orderBookData.ASKP_RSQN10}</td>
-            <td>{orderBookData.ASKP10}</td>
-            <td rowSpan={10}>
-              <table>
-                <tbody>
-                  <tr>
-                    <td>상한가</td>
-                    <td>76800</td>
-                  </tr>
-                  <tr>
-                    <td>하한가</td>
-                    <td>41400</td>
-                  </tr>
-                  <tr>
-                    <td>상승VI</td>
-                    <td>64100</td>
-                  </tr>
-                  <tr>
-                    <td>하강VI</td>
-                    <td>52300</td>
-                  </tr>
-                  <tr>
-                    <td>시작</td>
-                    <td>52300</td>
-                  </tr>
-                  <tr>
-                    <td>최고</td>
-                    <td>52300</td>
-                  </tr>
-                  <tr>
-                    <td>최저</td>
-                    <td>52300</td>
-                  </tr>
-                  <tr>
-                    <td>거래량</td>
-                    <td>3149만</td>
-                  </tr>
-                  <tr>
-                    <td>어제보다</td>
-                    <td>115.38%</td>
-                  </tr>
-                </tbody>
-              </table>
-              {/* <div>
-                <span>상한가</span>
-                <span>76800</span>
-              </div>
-              <div>
-                <span>하한가</span>
-                <span>41400</span>
-              </div>
-              <div>
-                <span>상승VI</span>
-                <span>64100</span>
-              </div>
-              <div>
-                <span>하강VI</span>
-                <span>52300</span>
-              </div>
-              <div>
-                <div>
-                  <span>시작</span>
-                  <span>58200</span>
-                </div>
-                <div>
-                  <span>최고</span>
-                  <span>58200</span>
-                </div>
-                <div>
-                  <span>최저</span>
-                  <span>58200</span>
-                </div>
-                <div>
-                  <span>거래량</span>
-                  <span>3149만</span>
-                </div>
-                <div>
-                  <span>어제보다</span>
-                  <span>115.38%</span>
-                </div>
-              </div> */}
-            </td>
-          </tr>
-          <tr>
-            <td>{orderBookData.ASKP_RSQN9}</td>
-            <td>{orderBookData.ASKP9}</td>
-          </tr>
-          <tr>
-            <td>{orderBookData.ASKP_RSQN8}</td>
-            <td>{orderBookData.ASKP8}</td>
-          </tr>
-          <tr>
-            <td>{orderBookData.ASKP_RSQN7}</td>
-            <td>{orderBookData.ASKP7}</td>
-          </tr>
-          <tr>
-            <td>{orderBookData.ASKP_RSQN6}</td>
-            <td>{orderBookData.ASKP6}</td>
-          </tr>
-          <tr>
-            <td>{orderBookData.ASKP_RSQN5}</td>
-            <td>{orderBookData.ASKP5}</td>
-          </tr>
-          <tr>
-            <td>{orderBookData.ASKP_RSQN4}</td>
-            <td>{orderBookData.ASKP4}</td>
-          </tr>
-          <tr>
-            <td>{orderBookData.ASKP_RSQN3}</td>
-            <td>{orderBookData.ASKP3}</td>
-          </tr>
-          <tr>
-            <td>{orderBookData.ASKP_RSQN2}</td>
-            <td>{orderBookData.ASKP2}</td>
-          </tr>
-          <tr>
-            <td>{orderBookData.ASKP_RSQN1}</td>
-            <td>{orderBookData.ASKP1}</td>
-          </tr>
+    <div className={styles.container}>
+      <div className={styles.categoryTabs}>
+        호가
+      </div>
+      <div className={styles.marketInfo}>
+        <p>시간외단일가 56,800원 <span className={styles.priceChange}>+0.35%</span></p>
+      </div>
+        <div className={styles.content}>
 
-          <tr>
-            <td rowSpan={10}>
-              <div>체결강도{tradingData[tradingData.length - 1]["CTTR"]}%</div>
-              <table>
-                <tbody>
-                  {tradingData.slice(-10).map((td, idx) => (
-                    <tr key={idx}>
-                      <td>{td.STCK_PRPR}</td>
-                      <td>{td.CNTG_VOL}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </td>
-            <td>{orderBookData.BIDP1}</td>
-            <td>{orderBookData.BIDP_RSQN1}</td>
-          </tr>
-          <tr>
-            <td>{orderBookData.BIDP2}</td>
-            <td>{orderBookData.BIDP_RSQN2}</td>
-          </tr>
-          <tr>
-            <td>{orderBookData.BIDP3}</td>
-            <td>{orderBookData.BIDP_RSQN3}</td>
-          </tr>
-          <tr>
-            <td>{orderBookData.BIDP4}</td>
-            <td>{orderBookData.BIDP_RSQN4}</td>
-          </tr>
-          <tr>
-            <td>{orderBookData.BIDP5}</td>
-            <td>{orderBookData.BIDP_RSQN5}</td>
-          </tr>
-          <tr>
-            <td>{orderBookData.BIDP6}</td>
-            <td>{orderBookData.BIDP_RSQN6}</td>
-          </tr>
-          <tr>
-            <td>{orderBookData.BIDP7}</td>
-            <td>{orderBookData.BIDP_RSQN7}</td>
-          </tr>
-          <tr>
-            <td>{orderBookData.BIDP8}</td>
-            <td>{orderBookData.BIDP_RSQN8}</td>
-          </tr>
-          <tr>
-            <td>{orderBookData.BIDP9}</td>
-            <td>{orderBookData.BIDP_RSQN9}</td>
-          </tr>
-          <tr>
-            <td>{orderBookData.BIDP10}</td>
-            <td>{orderBookData.BIDP_RSQN10}</td>
-          </tr>
-          <tr>
-            <td>
-              <p>판매대기</p> <p>{orderBookData.TOTAL_ASKP_RSQN}</p>
-            </td>
-            <td>정규장</td>
-            <td>
-              <p>구매대기</p> <p>{orderBookData.TOTAL_BIDP_RSQN}</p>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+          <table className={styles.orderTable}>
+            <thead>
+              <tr>
+                <th>매도잔량</th>
+                <th>가격</th>
+                <th>매수잔량</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* 매도 데이터 */}
+              {[...Array(10)].map((_, i) => (
+                <tr key={`ask-${i}`} className={styles.orderRow}>
+                  <td className={styles.askVolume}>{orderBookData[`ASKP_RSQN${10 - i}`].toLocaleString()}</td>
+                  <td
+                    className={styles.askPrice}
+                    style={{ color: orderBookData[`ASKP${10 - i}`] > currentPrice ? '#FF4F4F' : '#4881FF' }}
+                  >
+                    {orderBookData[`ASKP${10 - i}`].toLocaleString()}
+                  </td>
+                  {i === 0 && (
+                    <td className={styles.askInfo} rowSpan={10}>
+                      <div className={styles.askInfoContainer}>
+                        <div className={styles.detailInfo}>상한가<br></br>{maxAskPrice.toLocaleString()}</div>
+                        <div className={styles.detailInfo}>하한가<br></br>{minBidPrice.toLocaleString()}</div>
+                      </div>
+                      <div className={styles.askInfoContainer}>
+                        <div className={styles.detailInfo}>최고가<br></br>{maxPrice.toLocaleString()}</div>
+                        <div className={styles.detailInfo}>최저가<br></br>{minPrice.toLocaleString()}</div>
+                      </div>
+                      <div className={styles.detailInfo}>거래량<br></br>{quantity.toLocaleString()}</div>
+                    </td>
+                  )}
+                </tr>
+              ))}
+
+              {/* 구분선 */}
+              <tr style={{ borderTop: '3px solid #333'}}></tr>
+
+              {/* 매수 데이터 */}
+              {[...Array(10)].map((_, i) => (
+                <tr key={`bid-${i}`} className={styles.orderRow}>
+                  {i === 0 && (
+                    <td className={styles.bidInfo} rowSpan={10}></td>
+                  )}
+                  <td
+                    className={styles.bidPrice}
+                    style={{ color: orderBookData[`BIDP${i + 1}`] > currentPrice ? '#FF4F4F' : '#4881FF' }}
+                  >
+                    {orderBookData[`BIDP${i + 1}`].toLocaleString()}
+                  </td>
+                  <td className={styles.bidVolume}>{orderBookData[`BIDP_RSQN${i + 1}`].toLocaleString()}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>   
+        </div>
+        <div className={styles.summary}>
+          <p className={styles.askSummary}>판매대기 {orderBookData.TOTAL_ASKP_RSQN.toLocaleString()}</p>
+          <p className={styles.marketStatus}>정규장</p>
+          <p className={styles.bidSummary}>구매대기 {orderBookData.TOTAL_BIDP_RSQN.toLocaleString()}</p>
+        </div>
     </div>
   );
 };
