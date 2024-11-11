6f954ae75155fefd8564d8198d15815779011334
Thoughtful-Lover <isso8336@gmail.com>
2024-11-11 11:07:21 +0900

    :sparkles: Feat: stock info dummy data

--- frontend/src/home/stockRanking/StockRanking.tsx
+++ frontend/src/home/stockRanking/StockRanking.tsx
@@ -1,15 +1,22 @@
-
-import styles from './StockRanking.module.css'
-import StockInfo from './StockInfo'
+import styles from "./StockRanking.module.css";
+import StockInfo from "./StockInfo";
+import { useState } from "react";
 
 const StockRanking = () => {
   // const data = useIndexData()
-  return(
+  const [stockData] = useState({
+    dataRank: "1",
+    stockName: "삼성전자",
+    stockPrice: "100,000",
+    previousDayVersusPrice: "40,000",
+    previousDayVersus: "60",
+  });
+  return (
     <div className={styles.container}>
       StockRanking
-      <StockInfo />
+      <StockInfo {...stockData} />
     </div>
-  )
-}
+  );
+};
 
-export default StockRanking
+export default StockRanking;
