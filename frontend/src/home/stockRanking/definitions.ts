8204a785ee1371b29c8bd8b62dc8786746283415
Thoughtful-Lover <isso8336@gmail.com>
2024-11-11 10:43:08 +0900

    :sparkles: Feat: stock info type props

---
+++ frontend/src/home/stockRanking/definitions.ts
@@ -0,0 +1,8 @@
+interface IStockInfoProps {
+  dataRank: string;
+  logoLink: string;
+  stockName: string;
+  stockPrice: string;
+  previousDayVersusPrice: string;
+  previousDayVersus: string;
+}
