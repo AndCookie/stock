c001fa79c9abf5d9d5b06b016f0bcbfada5541d3
Thoughtful-Lover <isso8336@gmail.com>
2024-11-11 10:43:35 +0900

    :sparkles: Feat: stockinfo mounting to stockranking component

--- frontend/src/home/stockRanking/StockRanking.tsx
+++ frontend/src/home/stockRanking/StockRanking.tsx
@@ -1,11 +1,13 @@
 
 import styles from './StockRanking.module.css'
+import StockInfo from './StockInfo'
 
 const StockRanking = () => {
   // const data = useIndexData()
   return(
     <div className={styles.container}>
       StockRanking
+      <StockInfo />
     </div>
   )
 }
