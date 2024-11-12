fe31f046af6aa08879e2428e5b5c2cdea79b0431
이찬규 <sso756@naver.com>
2024-11-12 11:05:55 +0900

    :sparkles: Feat: user ranking mock data input

--- frontend/src/home/userRanking/UserRanking.tsx
+++ frontend/src/home/userRanking/UserRanking.tsx
@@ -1,15 +1,63 @@
-
-import styles from './UserRanking.module.css'
-import UserInfo from './UserInfo'
+import styles from './UserRanking.module.css';
+import UserInfo from './UserInfo';
+import { useState } from 'react';
 
 const UserRanking = () => {
-  // const data = useIndexData()
-  return(
-    <div className={styles.container}>
-      UserRanking
-      <UserInfo />
-    </div>
-  )
-}
+  const [weeklyRanking] = useState([
+    {
+      dataRank: '1',
+      userName: 'oodeng98',
+      profit: '65.8',
+    },
+    {
+      dataRank: '2',
+      userName: 'ssafyyong4',
+      profit: '56.3',
+    },
+    {
+      dataRank: '3',
+      userName: 'andcookie',
+      profit: '55.6',
+    },
+  ]);
+  const [totalRanking] = useState([
+    {
+      dataRank: '1',
+      userName: 'sso756',
+      profit: '396.4',
+    },
+    {
+      dataRank: '2',
+      userName: 'kokoz22',
+      profit: '312.2',
+    },
+    {
+      dataRank: '3',
+      userName: 'woojeanie',
+      profit: '251.2',
+    },
+  ]);
 
-export default UserRanking
+  return (
+    <div className={styles.container}>
+      <div>UserRanking</div>
+      <div className="">
+        <button className={`${'tabButton'} ${'activeTab'}`}>모의투자 랭킹</button>
+      </div>
+      <div className={styles.subTitle}>주간 수익률</div>
+      <div>
+        {weeklyRanking.map((user, index) => (
+          <UserInfo key={index} {...user} />
+        ))}
+      </div>
+      <div className={styles.subTitle}>전체 수익률</div>
+      <div>
+        {totalRanking.map((user, index) => (
+          <UserInfo key={index} {...user} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default UserRanking;
