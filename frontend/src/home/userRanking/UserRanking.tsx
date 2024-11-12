8eced5be5e87313a152f42a9c1b3b25407b0aece
이찬규 <sso756@naver.com>
2024-11-12 10:12:22 +0900

    :sparkles: Feat: mount userinfo to userranking page

--- frontend/src/home/userRanking/UserRanking.tsx
+++ frontend/src/home/userRanking/UserRanking.tsx
@@ -1,11 +1,13 @@
 
 import styles from './UserRanking.module.css'
+import UserInfo from './UserInfo'
 
 const UserRanking = () => {
   // const data = useIndexData()
   return(
     <div className={styles.container}>
       UserRanking
+      <UserInfo />
     </div>
   )
 }
