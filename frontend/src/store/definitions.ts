0430dec58af66cc1d0be167262eea1be427cc126
임광영 <kokoz22@naver.com>
2024-11-13 14:39:30 +0900

    [FE] Feat: Connect Websocket of Indicators - 거시경제 지표의 웹소켓을 연결했습니다. - IndicatorCard와 Rolling을 수정했습니다.

--- frontend/src/store/definitions.ts
+++ frontend/src/store/definitions.ts
@@ -38,7 +38,6 @@ export interface IIndexState {
   fetchIndexData: () => Promise<void>;
 }
 
-
 // type of Balance
 export interface IHolding {
   name: string;
@@ -124,4 +123,76 @@ export interface ITodayStockData {
 export interface ITodayStockState {
   minuteStockData: ITodayStockData[] | null;
   fetchMinuteStockData: () => Promise<void>;
+}
+
+// type of Websocket
+export interface IIndicatorData {
+  prpr_nmix: string;
+}
+
+export interface IOrderBookData {
+  ASKP1: number;  // 매도호가 1
+  ASKP2: number;  // 매도호가 2
+  ASKP3: number;  // 매도호가 3
+  ASKP4: number;  // 매도호가 4
+  ASKP5: number;  // 매도호가 5
+  ASKP6: number;  // 매도호가 6
+  ASKP7: number;  // 매도호가 7
+  ASKP8: number;  // 매도호가 8
+  ASKP9: number;  // 매도호가 9
+  ASKP10: number;  // 매도호가 10
+  BIDP1: number;  // 매수호가 1
+  BIDP2: number;  // 매수호가 2
+  BIDP3: number;  // 매수호가 3
+  BIDP4: number;  // 매수호가 4
+  BIDP5: number;  // 매수호가 5
+  BIDP6: number;  // 매수호가 6
+  BIDP7: number;  // 매수호가 7
+  BIDP8: number;  // 매수호가 8
+  BIDP9: number;  // 매수호가 9
+  BIDP10: number;  // 매수호가 10
+  ASKP_RSQN1: number;  // 매도호가 잔량 1
+  ASKP_RSQN2: number;  // 매도호가 잔량 2
+  ASKP_RSQN3: number;  // 매도호가 잔량 3
+  ASKP_RSQN4: number;  // 매도호가 잔량 4
+  ASKP_RSQN5: number;  // 매도호가 잔량 5
+  ASKP_RSQN6: number;  // 매도호가 잔량 6
+  ASKP_RSQN7: number;  // 매도호가 잔량 7
+  ASKP_RSQN8: number;  // 매도호가 잔량 8
+  ASKP_RSQN9: number;  // 매도호가 잔량 9
+  ASKP_RSQN10: number;  // 매도호가 잔량 10
+  BIDP_RSQN1: number;  // 매수호가 잔량 1
+  BIDP_RSQN2: number;  // 매수호가 잔량 2
+  BIDP_RSQN3: number;  // 매수호가 잔량 3
+  BIDP_RSQN4: number;  // 매수호가 잔량 4
+  BIDP_RSQN5: number;  // 매수호가 잔량 5
+  BIDP_RSQN6: number;  // 매수호가 잔량 6
+  BIDP_RSQN7: number;  // 매수호가 잔량 7
+  BIDP_RSQN8: number;  // 매수호가 잔량 8
+  BIDP_RSQN9: number;  // 매수호가 잔량 9
+  BIDP_RSQN10: number;  // 매수호가 잔량 10
+  TOTAL_ASKP_RSQN: number;  // 총 매도호가 잔량
+  TOTAL_BIDP_RSQN: number;  // 총 매수호가 잔량
+  [key: string]: number; // 추가하여 동적 키에 대한 타입 허용
+}
+
+export interface ITradingData {
+  STCK_CNTG_HOUR: string;  // 주식 체결 시간
+  STCK_PRPR: number;  // 주식 현재가
+  CNTG_VOL: number;  // 체결 거래량
+  ACML_VOL: number;  // 누적 거래량
+  CTTR: number;  // 체결강도
+  CCLD_DVSN: number;  // 체결구분
+}
+
+export interface ISocketStore {
+  kospiData: IIndicatorData | null;
+  kosdaqData: IIndicatorData | null;
+  setKospiData: (data: IIndicatorData) => void;
+  setKosdaqData: (data: IIndicatorData) => void;
+
+  // orderBookData: IOrderBookData | null;
+  // tradingData: ITradingData | null;
+  // setOrderBookData: (data: IOrderBookData) => void;
+  // setTradingData: (data: ITradingData) => void;
 }
