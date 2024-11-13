e2f340f8b4a38e7c5b31dfb10037234d9a21458f
이찬규 <sso756@naver.com>
2024-11-13 13:25:56 +0900

    :sparklse: Feat: auto complete drop down mount to search bar

--- frontend/src/common/nav/Search.tsx
+++ frontend/src/common/nav/Search.tsx
@@ -2,12 +2,25 @@ import { FormEvent } from 'react';
 import useInput from '../hooks/useInput';
 import styles from './Nav.module.css';
 import { fetchSearch } from './actions';
+import useDropdown from '../hooks/useDropdown';
+import Dropdown from '../components/DropDown';
 
 const Search = () => {
   const { inputValue, handleChange } = useInput('');
+
+  const {
+    handleDropDownKeyDown,
+    isFocus,
+    handleClickDropDownList,
+    dropDownList,
+    setDropDownItemIndex,
+    dropDownItemIndex,
+  } = useDropdown(inputValue);
+
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const res = await fetchSearch(inputValue);
+    console.log('Fetch Search Result:', res);
     if (res) {
       // TODO: navigate로 이동
       console.log(res);
@@ -21,7 +34,16 @@ const Search = () => {
           placeholder="주식, 메뉴, 종목코드를 검색하세요"
           value={inputValue}
           onChange={handleChange}
+          onKeyDown={handleDropDownKeyDown}
         />
+        {isFocus && (
+          <Dropdown
+            handleClickDropDownList={handleClickDropDownList}
+            dropDownList={dropDownList}
+            setDropDownItemIndex={setDropDownItemIndex}
+            dropDownItemIndex={dropDownItemIndex}
+          />
+        )}
       </form>
     </div>
   );
