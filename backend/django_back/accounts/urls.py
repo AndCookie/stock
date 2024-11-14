db146d49eac867349de2e6805968af17260b1d5f
정태완 <oodeng98@naver.com>
2024-11-14 10:52:01 +0900

    [BE] Feat: Balance - 잔고 조회 기능 추가

--- backend/django_back/accounts/urls.py
+++ backend/django_back/accounts/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('',include('dj_rest_auth.urls')),
     path('signup/',include('dj_rest_auth.registration.urls')),
-    # path('logout/',views.logout),
     path('social/',include('allauth.socialaccount.urls')),
     path('favorite-stock/', views.favorite_stock),    
+    path('balance/', views.balance),    
 ]
