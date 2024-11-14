8e49adb39a8d5a2b7d3d5b0fe171efc28693ee67
임광영 <kokoz22@naver.com>
2024-11-14 17:22:22 +0900

    [FE] Feat: Connect Stock Code - 종목 코드 stockCode에 따른 Axios와 WebSocket 연결

--- frontend/src/dashboard/chart/Chart.tsx
+++ frontend/src/dashboard/chart/Chart.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useRef, useState } from "react";
+import { useParams } from "react-router-dom";
 import { createChart, ISeriesApi, ColorType } from "lightweight-charts";
-import styles from './Chart.module.css';
 
 import { usePastStockStore } from "../../store/usePastStockStore";
-import useSocketStore, { sendMessage } from "../../store/useSocketStore";
+import useSocketStore from "../../store/useSocketStore";
 
+import styles from './Chart.module.css';
 import { COLORS } from "../../common/utils";
 import { IChartStockData, IChartVolumeData } from "./definitions";
 
@@ -21,10 +22,14 @@ const Chart = () => {
 
   const [selectedPeriodCode, setSelectedPeriodCode] = useState('D');
 
+  const { stockCode } = useParams();
+
   useEffect(() => {
+    if (!stockCode) return;
+
     setChartStockData(null);
     setChartVolumeData(null);
-    fetchPastStockData("005930", selectedPeriodCode);
+    fetchPastStockData(stockCode, selectedPeriodCode);
   }, [selectedPeriodCode])
 
   // chartStockData 업데이트
@@ -68,9 +73,6 @@ const Chart = () => {
   // 차트 초기화
   useEffect(() => {
     if (!chartContainerRef.current || !chartStockData || !chartVolumeData) return;
-
-    // 웹 소켓 종목코드 전송
-    sendMessage({ stock_code: "005930" });
 
     const chartOptions = {
       width: chartContainerRef.current.clientWidth,
