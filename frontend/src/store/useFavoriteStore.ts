a5435ee7fe577ce90503c06860ab9cc33088e538
이찬규 <sso756@naver.com>
2024-11-15 14:26:52 +0900

    [FE] Feat: favorite stock post delete complete

--- frontend/src/store/useFavoriteStore.ts
+++ frontend/src/store/useFavoriteStore.ts
@@ -1,6 +1,6 @@
-import axios from "axios";
-import { create } from "zustand";
-import { IFavoriteState } from "./definitions";
+import axios from 'axios';
+import { create } from 'zustand';
+import { IFavoriteState } from './definitions';
 
 const baseURL = import.meta.env.VITE_LOCAL_BASEURL;
 
@@ -9,10 +9,44 @@ export const useFavoriteStore = create<IFavoriteState>((set) => ({
 
   fetchFavoriteData: async () => {
     try {
-      const response = await axios.get(baseURL + "account/favorite");
+      const response = await axios.get(`${baseURL}account/favorite`);
       set(() => ({
         favoriteData: response.data,
       }));
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  postFavoriteData: async (stockCode: string) => {
+    try {
+      const response = await axios.post(
+        `${baseURL}account/favorite`,
+        { stock_code: stockCode },
+        {
+          headers: {
+            'Authorization': 'Bearer YOUR_TOKEN_HERE',
+          },
+        }
+      );
+      console.log('Success :', response.data)
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  deleteFavoriteData: async (stockCode: string) => {
+    try {
+      const response = await axios.delete(`${baseURL}account/favorite`, {
+        headers: {
+          'Authorization': 'Bearer YOUR_TOKEN_HERE',
+        },
+        data: {
+          stock_code: stockCode,
+        },
+      });
+
+      console.log('Success :', response.data)
     } catch (error) {
       console.log(error);
     }
