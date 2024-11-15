b645b089a643f053cc6e09c9a8602602f4cb0128
AndCookie <jamie9@naver.com>
2024-11-15 15:45:19 +0900

    [FE] Design: home-ranking - stockRanking - UserRanking - Account

--- frontend/src/home/stockRanking/StockInfo.tsx
+++ frontend/src/home/stockRanking/StockInfo.tsx
@@ -1,6 +1,6 @@
-import { IStockInfoProps } from "./definitions";
+import { IStockInfoProps } from './definitions';
 
-import styles from "./StockInfo.module.css";
+import styles from './StockInfo.module.css';
 
 const StockInfo: React.FC<IStockInfoProps> = ({ stockRankingData }) => {
   return (
@@ -11,12 +11,21 @@ const StockInfo: React.FC<IStockInfoProps> = ({ stockRankingData }) => {
         <div className={styles.stockName}>{stockRankingData.hts_kor_isnm}</div>
       </div>
       <div className={styles.priceData}>
-        <div>{Number(stockRankingData.stck_prpr).toLocaleString()}원</div>
+        <div className={styles.price}>{Number(stockRankingData.stck_prpr).toLocaleString()}원</div>
         <div
-          className={`${styles.previousDayVersusData} ${Number(stockRankingData.prdy_vrss) > 0 ? styles.previousDayVersusPricePlus : styles.previousDayVersusPriceMinus}`}
+          className={`${styles.previousDayVersusData} ${
+            Number(stockRankingData.prdy_vrss) > 0
+              ? styles.previousDayVersusPricePlus
+              : styles.previousDayVersusPriceMinus
+          }`}
         >
-          {Number(stockRankingData.prdy_vrss) > 0 ? "▲" : Number(stockRankingData.prdy_vrss) < 0 ? "▼" : ""} {Math.abs(Number(stockRankingData.prdy_vrss)).toLocaleString()}원
-          ({Number(stockRankingData.prdy_ctrt).toFixed(1).toLocaleString()}%)
+          {Number(stockRankingData.prdy_vrss) > 0
+            ? '▲'
+            : Number(stockRankingData.prdy_vrss) < 0
+            ? '▼'
+            : ''}{' '}
+          {Math.abs(Number(stockRankingData.prdy_vrss)).toLocaleString()}원 (
+          {Number(stockRankingData.prdy_ctrt).toFixed(1).toLocaleString()}%)
         </div>
       </div>
     </div>
--- frontend/src/home/stockRanking/StockRanking.tsx
+++ frontend/src/home/stockRanking/StockRanking.tsx
@@ -8,22 +8,22 @@ import { IStockRankingData } from './definitions';
 import styles from './StockRanking.module.css';
 
 const StockRanking = () => {
-  const [currentTime, setCurrentTime] = useState("");
-  const [selectedCategory, setSelectedCategory] = useState<string>("거래 대금");
+  const [currentTime, setCurrentTime] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState<string>('거래 대금');
   const [renderedStockRankingData, setRenderedStockData] = useState([]);
 
-  const categories = ["거래 대금", "거래량", "급상승", "급하락"];
+  const categories = ['거래 대금', '거래량', '급상승', '급하락'];
   const categoryToKeyword: { [key: string]: string } = {
-    "거래 대금": "amount",
-    "거래량": "volume",
-    "급상승": "advance",
-    "급하락": "decline",
+    '거래 대금': 'amount',
+    거래량: 'volume',
+    급상승: 'advance',
+    급하락: 'decline',
   };
 
   const updateCurrentTime = () => {
     const now = new Date();
-    const hours = now.getHours().toString().padStart(2, "0");
-    const minutes = now.getMinutes().toString().padStart(2, "0");
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
     setCurrentTime(`오늘 ${hours}:${minutes} 기준`);
   };
 
@@ -53,12 +53,12 @@ const StockRanking = () => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.categoryTabs}>
+      <div className={styles.categoryTab}>
         종목 랭킹
         <div className={styles.time}>{currentTime}</div>
       </div>
 
-      <div className="categoryTabs">
+      <div className={styles.categoryTabs}>
         {categories.map((category) => (
           <button
             key={category}
@@ -71,9 +71,11 @@ const StockRanking = () => {
       </div>
 
       <div className={styles.content}>
-        {renderedStockRankingData.slice(0, 5).map((stockRankingData: IStockRankingData, index: number) => (
-          <StockInfo key={index} stockRankingData={stockRankingData} />
-        ))}
+        {renderedStockRankingData
+          .slice(0, 5)
+          .map((stockRankingData: IStockRankingData, index: number) => (
+            <StockInfo key={index} stockRankingData={stockRankingData} />
+          ))}
       </div>
     </div>
   );
