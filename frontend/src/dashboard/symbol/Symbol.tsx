5316b11240e7f0a40c34244fdb2c02585b1c43c1
이찬규 <sso756@naver.com>
2024-11-15 17:25:58 +0900

    [FE] Refactor: dashboard symbol favoriteStock api stockCode refactor

--- frontend/src/dashboard/symbol/Symbol.tsx
+++ frontend/src/dashboard/symbol/Symbol.tsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 import codeToName from '../../assets/codeToName.json';
-import { usePastStockStore } from "../../store/usePastStockStore";
-import { useMinuteStockStore } from "../../store/useMinuteStockStore";
-import useSocketStore from "../../store/useSocketStore";
+import { usePastStockStore } from '../../store/usePastStockStore';
+import { useMinuteStockStore } from '../../store/useMinuteStockStore';
+import useSocketStore from '../../store/useSocketStore';
 import { useFavoriteStore } from '../../store/useFavoriteStore';
 
 import { IWidgetComponentProps } from '../../common/definitions';
@@ -15,8 +15,12 @@ const Symbol = ({ setIsDraggable }: IWidgetComponentProps) => {
   const { stockCode } = useParams();
 
   // TODO: 실제 data를 넣어주세요
-  const name = stockCode ? codeToName[stockCode] : "";
-  const { industry, companyDetail, favorite } = { industry: "IT", companyDetail: "반도체와반도체장비", favorite: false };
+  const name = stockCode ? codeToName[stockCode] : '';
+  const { industry, companyDetail, favorite } = {
+    industry: 'IT',
+    companyDetail: '반도체와반도체장비',
+    favorite: false,
+  };
 
   const { pastStockData, yesterdayStockData } = usePastStockStore();
   const { minuteStockData } = useMinuteStockStore();
@@ -26,22 +30,30 @@ const Symbol = ({ setIsDraggable }: IWidgetComponentProps) => {
   const [renderedChangeValue, setRenderedChangeValue] = useState(0);
   const [renderedChangeRate, setRenderedChangeRate] = useState(0);
   const [isFavorite, setIsFavorite] = useState<boolean>(favorite || false);
-  
+
   const favoriteData = useFavoriteStore((state) => state.favoriteData);
   const fetchFavoriteData = useFavoriteStore((state) => state.fetchFavoriteData);
   const postFavoriteData = useFavoriteStore((state) => state.postFavoriteData);
   const deleteFavoriteData = useFavoriteStore((state) => state.deleteFavoriteData);
-  
+
   useEffect(() => {
     if (!pastStockData || !minuteStockData) return;
 
     setRenderedValue(Number(minuteStockData![minuteStockData!.length - 1].stck_prpr));
-    setRenderedChangeValue(Number(minuteStockData![minuteStockData!.length - 1].stck_prpr) - Number(yesterdayStockData));
-    setRenderedChangeRate((Number(minuteStockData![minuteStockData!.length - 1].stck_prpr) - Number(yesterdayStockData)) / Number(yesterdayStockData) * 100);
-  }, [pastStockData])
+    setRenderedChangeValue(
+      Number(minuteStockData![minuteStockData!.length - 1].stck_prpr) - Number(yesterdayStockData)
+    );
+    setRenderedChangeRate(
+      ((Number(minuteStockData![minuteStockData!.length - 1].stck_prpr) -
+        Number(yesterdayStockData)) /
+        Number(yesterdayStockData)) *
+        100
+    );
+  }, [pastStockData]);
 
   // 여기부터 코드 다시 입력
   useEffect(() => {
+    fetchFavoriteData();
     // 이 현재 종목 코드를 어디서 가져오ㄴ가 문제다 !!
     if (favoriteData && '현재 종목 코드' in favoriteData) {
       setIsFavorite(true);
@@ -52,20 +64,23 @@ const Symbol = ({ setIsDraggable }: IWidgetComponentProps) => {
 
   useEffect(() => {
     if (!tradingData) return;
-    
+
     setRenderedValue(Number(tradingData.STCK_PRPR));
     setRenderedChangeValue(Number(tradingData.STCK_PRPR) - Number(yesterdayStockData));
-    setRenderedChangeRate((Number(tradingData.STCK_PRPR) - Number(yesterdayStockData)) / Number(yesterdayStockData) * 100);
-  }, [tradingData])
+    setRenderedChangeRate(
+      ((Number(tradingData.STCK_PRPR) - Number(yesterdayStockData)) / Number(yesterdayStockData)) *
+        100
+    );
+  }, [tradingData]);
 
   // TODO: 비즈니스 로직이니 분리하세요
   const toggleFavorite = () => {
     // TODO: post 요청을 통해 서버 상태 업데이트
     setIsFavorite((prev) => !prev);
     if (!isFavorite) {
-      postFavoriteData('005930');
+      postFavoriteData(stockCode);
     } else {
-      deleteFavoriteData('005930');
+      deleteFavoriteData(stockCode);
     }
   };
 
