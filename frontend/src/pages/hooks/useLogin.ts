4035c412bcfb159ea1ff2c83f163bab92932136c
이찬규 <sso756@naver.com>
2024-11-16 13:21:51 +0900

    [FE] Feat: login token save

--- frontend/src/pages/hooks/useLogin.ts
+++ frontend/src/pages/hooks/useLogin.ts
@@ -1,17 +1,24 @@
 import axios from 'axios';
+import { create } from 'zustand';
 
 const baseURL = import.meta.env.VITE_LOCAL_BASEURL;
 
-export const useLogin = async () => {
+export const useLogin = create((set) => ({
+  loginToken: null,
+
+  login: async () => {
     try {
-        const response = await axios.post(`${baseURL}accounts/login/`, {
-            // 원래는 최소한 .env 파일에 넣는 정성이라도 보여줘야 하는데
-            // 나중에 코드 반출할 때는 이 부분 참고할 것
-            username: 'admin',
-            password: 'rkskekfk'
-        })
-        const result = response.data
+      const response = await axios.post(`${baseURL}accounts/login/`, {
+        // 원래는 최소한 .env 파일에 넣는 정성이라도 보여줘야 하는데
+        // 나중에 코드 반출할 때는 이 부분 참고할 것
+        username: 'admin',
+        password: 'rkskekfk',
+      });
+      set(() => ({
+        loginToken: response.data.key,
+      }));
     } catch (error) {
-        console.log(error)
+      console.log(error);
     }
-}
+  },
+}));
