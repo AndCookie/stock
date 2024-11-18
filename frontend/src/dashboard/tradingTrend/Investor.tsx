e767624c31e8921df6b4547128e35e98c1e7b86c
임광영 <kokoz22@naver.com>
2024-11-18 10:20:53 +0900

    [FE] Feat: Connect Axios of Investor - 투자자 부분 Axios를 연결했습니다.

--- frontend/src/dashboard/tradingTrend/Investor.tsx
+++ frontend/src/dashboard/tradingTrend/Investor.tsx
@@ -4,11 +4,11 @@ import { useEffect, useState } from "react";
 import fetchTraderTrend from "./hooks/fetchTraderTrend";
 import { IInvestor } from "./definitions";
 
-// import styles from "./TradingTrend.module.css";
+import styles from "./TradingTrend.module.css";
 
 const Investor = () => {
   const { stockCode } = useParams();
-  const [renderedInvestorData, setRenderedInvestorData] = useState<IInvestor | null>(null);
+  const [renderedInvestorData, setRenderedInvestorData] = useState<IInvestor[] | null>(null);
 
   useEffect(() => {
     if (!stockCode) return;
@@ -29,40 +29,38 @@ const Investor = () => {
     console.log(renderedInvestorData);
   }, [renderedInvestorData])
 
-  return <div />;
+  if (!renderedInvestorData) return <div />;
 
-  // if (!renderedInvestorData) return <div />;
-
-  // return (
-  //   <div className={styles.subContent}>
-  //     <table className={styles.investorTable}>
-  //       <thead>
-  //         <tr>
-  //           <th className={styles.thDate}>일자</th>
-  //           <th className={styles.thInvest}>외국인</th>
-  //           <th className={styles.thInvest}>기관계</th>
-  //           <th className={styles.thInvest}>개인</th>
-  //         </tr>
-  //       </thead>
-  //       <tbody>
-  //         {renderedInvestorData.map((item, index) => (
-  //             <tr key={index}>
-  //               <td>{item.date}</td>
-  //               <td className={item.foreigner >= 0 ? styles.positive : styles.negative}>
-  //                 {item.foreigner.toLocaleString()}
-  //               </td>
-  //               <td className={item.corporate >= 0 ? styles.positive : styles.negative}>
-  //                 {item.corporate.toLocaleString()}
-  //               </td>
-  //               <td className={item.individual >= 0 ? styles.positive : styles.negative}>
-  //                 {item.individual.toLocaleString()}
-  //               </td>
-  //             </tr>
-  //           ))}
-  //       </tbody>
-  //     </table>
-  //   </div>
-  // );
+  return (
+    <div className={styles.subContent}>
+      <table className={styles.investorTable}>
+        <thead>
+          <tr>
+            <th className={styles.thDate}>일자</th>
+            <th className={styles.thInvest}>외국인</th>
+            <th className={styles.thInvest}>기관계</th>
+            <th className={styles.thInvest}>개인</th>
+          </tr>
+        </thead>
+        <tbody>
+          {renderedInvestorData.slice(1, renderedInvestorData.length).map((item, index) => (
+              <tr key={index}>
+                <td>{item.stck_bsop_date.slice(0, 4)}-{item.stck_bsop_date.slice(4, 6)}-{item.stck_bsop_date.slice(6, 8)}</td>
+                <td className={Number(item.frgn_ntby_qty) >= 0 ? styles.positive : styles.negative}>
+                  {Number(item.frgn_ntby_qty).toLocaleString()}
+                </td>
+                <td className={Number(item.orgn_ntby_qty) >= 0 ? styles.positive : styles.negative}>
+                  {Number(item.orgn_ntby_qty).toLocaleString()}
+                </td>
+                <td className={Number(item.prsn_ntby_qty) >= 0 ? styles.positive : styles.negative}>
+                  {Number(item.prsn_ntby_qty).toLocaleString()}
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Investor;
