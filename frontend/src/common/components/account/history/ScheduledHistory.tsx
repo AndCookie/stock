b4f0425d0c871b3f0fd6193077e0035c84d6108f
이찬규 <sso756@naver.com>
2024-11-18 22:04:38 +0900

    [FE] Feat: ScheduldedHistory delete logic

--- frontend/src/common/components/account/history/ScheduledHistory.tsx
+++ frontend/src/common/components/account/history/ScheduledHistory.tsx
@@ -1,19 +1,30 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import { useScheduledHistoryStore } from "../../../../store/useHistoryStore";
-import { IScheduledHistoryData } from "../../../../store/definitions";
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { useScheduledHistoryStore } from '../../../../store/useHistoryStore';
+import { IScheduledHistoryData } from '../../../../store/definitions';
 
-const ScheduledHistory: React.FC<{ filter: string; isMyPage: boolean }> = ({ filter, isMyPage }) => {
+const ScheduledHistory: React.FC<{ filter: string; isMyPage: boolean }> = ({
+  filter,
+  isMyPage,
+}) => {
   // isDetailPage가 true면 filter가 stockCode
   // isDetailPage가 false면 메인페이지랑 마이페이지
-  const isDetailPage = filter !== "ALL";
+  const isDetailPage = filter !== 'ALL';
 
-    // TODO 이건 나중에 디자인 끝나고 지우세요!!
-    console.log("Temp", isMyPage)
-    console.log("Temp", isDetailPage)  
+  // TODO 이건 나중에 디자인 끝나고 지우세요!!
+  console.log('Temp', isMyPage);
+  console.log('Temp', isDetailPage);
 
-  const { scheduledHistoryData, fetchScheduledHistoryData } = useScheduledHistoryStore();
+  const { scheduledHistoryData, fetchScheduledHistoryData, deleteArray } =
+    useScheduledHistoryStore();
   const [filteredHistoryData, setFilteredHistoryData] = useState<IScheduledHistoryData[]>([]);
+
+  const deleteScheduledHistoryData = (index: number) => {
+    if (!deleteArray.includes(index)) {
+      deleteArray.push(index);
+    }
+    console.log(deleteArray);
+  };
 
   useEffect(() => {
     if (!scheduledHistoryData) {
@@ -23,38 +34,41 @@ const ScheduledHistory: React.FC<{ filter: string; isMyPage: boolean }> = ({ fil
 
   useEffect(() => {
     if (scheduledHistoryData) {
-      console.log("### scheduledHistoryData: ", scheduledHistoryData)
-      if (filter === "ALL") {
+      console.log('### scheduledHistoryData: ', scheduledHistoryData);
+      if (filter === 'ALL') {
         setFilteredHistoryData(scheduledHistoryData);
       } else {
-        setFilteredHistoryData(
-          scheduledHistoryData.filter(({ pdno }) => pdno === filter)
-        );
+        setFilteredHistoryData(scheduledHistoryData.filter(({ pdno }) => pdno === filter));
       }
     }
-  }, [scheduledHistoryData]);
-
+  }, [scheduledHistoryData, deleteArray]);
 
   const renderHistoryData = (historyData: IScheduledHistoryData[]) =>
     historyData.length > 0 ? (
-      historyData.map((order, index) => (
-        <div key={index}>
-          <span>{order.prdt_name}</span>
-          <span>현재가 {order.tar_pr}원 {order.sll_buy_dvsn_cd === "BUY" ? "이하" : "이상"}일 때</span>
-          <span>{order.sll_buy_dvsn_cd === "BUY" ? "구매" : "판매"} {order.ord_qty}주</span>{" "}
-          ·{" "}
-          <span>
-            {order.ord_unpr === 0
-              ? "시장가 주문"
-              : `지정가 주문 · 주문가 ${order.ord_unpr}원`}
-          </span>
-        </div>
-      ))
+      historyData.map((order, index) =>
+        !deleteArray.includes(index) ? (
+          <div key={index}>
+            <span>{order.prdt_name}</span>
+            <span>
+              현재가 {order.tar_pr}원 {order.sll_buy_dvsn_cd === 'BUY' ? '이하' : '이상'}일 때
+            </span>
+            <span>
+              {order.sll_buy_dvsn_cd === 'BUY' ? '구매' : '판매'} {order.ord_qty}주
+            </span>{' '}
+            ·{' '}
+            <span>
+              {order.ord_unpr === 0 ? '시장가 주문' : `지정가 주문 · 주문가 ${order.ord_unpr}원`}
+            </span>
+            {/* 대기 주문 취소 */}
+            <div onClick={() => deleteScheduledHistoryData(index)}>X</div>
+          </div>
+        ) : null
+      )
     ) : (
       <p>조건내역이 없습니다</p>
     );
 
-  return(
+  return (
     <div>
       <div className="content">
         <div className="section">
@@ -66,4 +80,4 @@ const ScheduledHistory: React.FC<{ filter: string; isMyPage: boolean }> = ({ fil
   );
 };
 
-export default ScheduledHistory
+export default ScheduledHistory;
