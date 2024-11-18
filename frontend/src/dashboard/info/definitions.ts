b8f77423678f8d56dc1f59062fab4e88d288153f
이찬규 <sso756@naver.com>
2024-11-18 17:51:21 +0900

    [FE] News, Item component type setting

--- frontend/src/dashboard/info/definitions.ts
+++ frontend/src/dashboard/info/definitions.ts
@@ -7,12 +7,14 @@ export interface ICompanyData {
   description: string;
 }
 
+export interface NewsItem {
+  hts_pbnt_titl_cntt: string; // 공시 제목 내용
+  dorg: string; // 자료원
+  data_dt: string; // 일자
+}
+
 // 필요시에는 뉴스랑 공시 분리할 것
 export interface ICompanyNewsDisclosure {
-  // HTS 공시 제목 내용
-  hts_pbnt_titl_cntt: string;
-  // 자료원
-  dorg: string;
-  // 일자
-  data_dt: string;
+  stockCode: string; // 주식 코드
+  news?: NewsItem[]; // 뉴스 항목 배열
 }
